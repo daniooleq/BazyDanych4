b893f32e4a69d33569f76a1f171c0ebc5b05117a
daniooleq <90752594+daniooleq@users.noreply.github.com>
2024-02-04 14:44:51 +0100

    tlumaczenie C.php

--- css->flexbox/C.php
+++ css->flexbox/C.php
@@ -1,4 +1,7 @@
 <?php
+
+//.PHP_EOL to cos w stylu <br> lub shift+enter - oznacza koniec lini ktory jest bezpieczny dla wielu systemow operacyjnych, zeby nie bylo bledow i zeby ladnie wygladalo. 
+
 session_start();
 $file = fopen("D.xml", "w+");
 $xml = "<czlowiek>".PHP_EOL;
@@ -25,6 +28,5 @@ $xml .= $_SESSION["g1"];
 $xml .= "</dane>".PHP_EOL;
 $xml .="</czlowiek>".PHP_EOL;          
 fwrite($file,$xml);
-
 fclose($file);
 ?>
