32216bf574b182088c72a162af6ee38d93956c9c
daniooleq <90752594+daniooleq@users.noreply.github.com>
2024-02-19 15:04:17 +0100

    Add files via upload

---
+++ sekcje/strona.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+     <title>Przykładowy tytuł</title>
+     <link rel="stylesheet" href="cos.css">
+  </head>
+  <body>
+    <header id="naglowek1">
+        
+      <div class='dziecko'  id="naglowek">strona danielka</div>
+
+<nav id="nawigacja1">
+     <div class='dziecko' id="nawigacja">Menu nawigacyjne</div> </nav>
+ </header>
+
+    <section id="sekcja">         
+       <article id="TRESC"><img src="kiepski.jpg"></article>
+       <article id="TRESC"><img src="pazdzioch.jpg"></article>
+       <article id="TRESC"><img src="boczek.jpg"></article>
+    </section>
+
+   <aside id="INFORMACJE"><div id="info" align="right"> Panel boczny</div></aside>  <br>
+
+   <footer id="STOPKA"><div id="stopka2">Stopka serwisu</div></footer>
+  </body>
+</html>
+
